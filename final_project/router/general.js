af195823965688e86f45a2d853f15def3e0fc6e2
tjch-o <e0929841@u.nus.edu>
2024-05-13 19:18:04 +0800

    Refactor code with promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,46 +24,80 @@ public_users.post('/register', (req, res) => {
 })
 
 // Get the book list available in the shop
-public_users.get('/', function (req, res) {
-  return res.status(200).json({ books })
+public_users.get('/', async function (req, res) {
+  try {
+    const retrievedBooks = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(books)
+      }, 500)
+    })
+
+    return res.status(200).json({ retrievedBooks })
+  } catch (error) {
+    return res.status(500).send('An error occurred while fetching books')
+  }
 })
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
   const { isbn } = req.params
 
-  if (!books[isbn]) {
-    return res.status(404).json({ message: 'Book not found' })
+  try {
+    const retrievedBooks = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(books)
+      }, 500)
+    })
+
+    if (!retrievedBooks[isbn]) {
+      return res.status(404).json({ message: 'Book not found' })
+    }
+    return res.status(200).json({ book: retrievedBooks[isbn] })
+  } catch (error) {
+    return res.status(500).send('An error occurred while fetching books with this ISBN')
   }
-  return res.status(200).json({ book: books[isbn] })
 })
 
 // Get book details based on author
-public_users.get('/author/:author', function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
   const { author } = req.params
-  const booksFromAuthor = Object.values(books).filter(
-    book => book.author == author
-  )
 
-  if (!booksFromAuthor) {
-    return res.status(404).json({ message: 'Books by author not found' })
+  try {
+    const booksFromAuthor = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(Object.values(books).filter(book => book.author == author))
+      }, 500)
+    })
+
+    if (!booksFromAuthor) {
+      return res.status(404).json({ message: 'Books by author not found' })
+    }
+  
+    return res.status(200).json({ books: booksFromAuthor })
+  } catch (error) {
+    return res.status(500).send('An error occurred while fetching books from this author')
   }
-
-  return res.status(200).json({ books: booksFromAuthor })
 })
 
 // Get all books based on title
-public_users.get('/title/:title', function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
   const { title } = req.params
-  const booksWithTitle = Object.values(books).filter(
-    book => book.title == title
-  )
 
-  if (!booksWithTitle) {
-    return res.status(404).json({ message: 'Books by author not found' })
+  try {
+    const booksWithTitle = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(Object.values(books).filter(book => book.title == title))
+      }, 500)
+    })
+
+    if (!booksWithTitle) {
+      return res.status(404).json({ message: 'Books by author not found' })
+    }
+  
+    return res.status(200).json({ books: booksWithTitle })
+  } catch (error) {
+    return res.status(500).send('An error occurred while fetching books with this title')
   }
-
-  return res.status(200).json({ books: booksWithTitle })
 })
 
 // Get book reviews for a book based on ISBN
